7f4e85cac995ae28e2708aa9e3e6aa69b55abb52
cho <websera12@gmail.com>
2023-10-22 20:12:53 +0900

    공지사항 css, js 수정 / 회원탈퇴 html, css 추가

--- front/js/notice.js
+++ front/js/notice.js
@@ -3,7 +3,11 @@ $(function(){
     $('.noticeItem').click(function(){
         let bro = $(this).siblings();
         let cont = $(this).children('.contentBox');
-        bro.children('.contentBox').hide();
-        cont.slideToggle(300);
+        bro.children('.contentBox').slideUp(300,function(){
+            $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_down</span>');
+        });
+        cont.slideDown(300, function(){
+            $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_up</span>');
+        });
     });
 });
