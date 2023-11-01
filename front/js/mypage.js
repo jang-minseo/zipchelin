3b5b4faa3bade40740ba2c336831613912c1e824
DESKTOP-NKT48T6\subin <websera12@gmail.com>
2023-11-01 17:09:33 +0900

    마이페이지 내가쓴글, 내 댓글 - 체크박스 수정2

--- front/js/mypage.js
+++ front/js/mypage.js
@@ -1,33 +1,28 @@
 $(function(){
-    //버튼 그룹 보기, 숨기기
-    $('#showChk').click(function(){
-        $(this).parent().addClass('hide').next().removeClass('hide');
-        $('.myItem .chkWrap').css('display','block');
+    //리스트 선택 - 체크박스
+    /* $('#showChk').on('click',function(){
+        $(this).html('취소').attr('id','chkCancle');
+        $('#allChk, #listDel,.chkWrap').css('display','block');
+    }); */
+
+    //리스트 선택 취소 - 체크박스 취소
+    /* $('#chkCancle').click(function(){
+        $(this).html('<button class="btnBdGray" id="showChk">선택</button>');
+    }); */
+    $(document).on("click", "#showChk", function() {
+        $(this).css('display','none');
+        $('#allChk, #listDel, #chkCancle, .chkWrap').css('display','block');
     });
 
     $('#chkCancle').click(function(){
-        $(this).parent().addClass('hide').prev().removeClass('hide');
+        $('#allChk, #listDel, #chkCancle, .chkWrap').css('display','none');
+        $('#showChk').css('display','block');
     });
 
     //체크박스 전체 선택
-    $("#allChk").click(function() {
-		if($("#allChk").is(":checked")){
-            $("input[name=chkItem]").prop("checked", true);
-        }else {
-            $("input[name=chkItem]").prop("checked", false);
-        }
-	});
-
-	$("input[name=chkItem]").click(function() {
-		var total = $("input[name=chkItem]").length;
-		var checked = $("input[name=chkItem]:checked").length;
-
-		if(total != checked){
-            $("#allChk").prop("checked", false);
-        } else{
-            $("#allChk").prop("checked", true); 
-        }     
-	});
+    $('#allChk').click(function(){   
+        $('.chkBox').prop('checked', true);       
+    });
 
     //삭제 버튼 - 모달
     $('#listDel').click(function(){
@@ -38,10 +33,10 @@ $(function(){
     //내가 쓴글 - 탭
     $('ul.tabGroup a').click(function(){
         var tab_id = $(this).attr('data-tab');
-    
+     
         $('ul.tabGroup a').removeClass('active');
         $('.tabCont').removeClass('show');
-    
+     
         $(this).addClass('active');
         $("#"+tab_id).addClass('show');
 
