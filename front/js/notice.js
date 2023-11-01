1ae7000acdf2cb36659f092f9b8cce9d13591a7d
cho <websera12@gmail.com>
2023-11-02 00:40:59 +0900

    공지사항 토글 수정 / 내가쓴글, 내 댓글 a태그 리스트 감싸기

--- front/js/notice.js
+++ front/js/notice.js
@@ -3,11 +3,25 @@ $(function(){
     $('.noticeItem').click(function(){
         let bro = $(this).siblings();
         let cont = $(this).children('.contentBox');
+        let tog = true;
+        
         bro.children('.contentBox').slideUp(300,function(){
             $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_down</span>');
+            tog = false;
         });
         cont.slideDown(300, function(){
             $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_up</span>');
+            tog = true;
         });
+
+        if(tog == true){
+            $(this).children('.contentBox').slideUp(300, function(){
+                $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_down</span>');
+            });
+        }else{
+            $(this).children('.contentBox').slideDown(300, function(){
+                $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_up</span>');
+            });
+        }
     });
 });
