59486ab83ab07bd6630a42c43b4841a0577f701a
DESKTOP-NKT48T6\subin <websera12@gmail.com>
2023-11-23 19:30:47 +0900

    mypage패키지 생성, 컨트롤러

---
+++ src/main/java/zipchelin/mypage/MypageController.java
@@ -0,0 +1,43 @@
+package zipchelin.mypage;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mypage/*")
+public class MypageController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nextPage="";
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		String action=request.getPathInfo();	//요청명을 가져옴
+		try {
+			if(action==null || action.equals("/mypage.do")) {				
+				nextPage="/WEB-INF/index.jsp";
+			}else if(action.equals("/mypage_pwdConfirm.do")) {
+				nextPage="/WEB-INF/views/main/mypage/mypwdConfirm.jsp";			
+			}
+			
+		}catch (Exception e) {
+			System.out.println("요청 처리 중 에러!!");
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+		dispatcher.forward(request, response);
+	}
+}
