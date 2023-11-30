a41d247ebb177f6bff01dd03830d423139ee561e
DESKTOP-NKT48T6\subin <websera12@gmail.com>
2023-11-30 21:35:44 +0900

    공지사항 리스트 토글 수정 / 메인 리스트 간격 맞춤

--- front/js/notice.js
+++ front/js/notice.js
@@ -1,7 +1,14 @@
 $(function(){
     //공지사항 리스트 토글
     $('.noticeItem').click(function(){
-        let bro = $(this).siblings();
+        let chk = $(this).hasClass('active');
+		if(chk){
+			$(this).removeClass('active');
+		}else{
+			$('.noticeItem').removeClass('active');
+			$(this).addClass('active');
+		}
+        /*let bro = $(this).siblings();
         let cont = $(this).children('.contentBox');
         let tog = true;
         
@@ -22,6 +29,6 @@ $(function(){
             $(this).children('.contentBox').slideDown(300, function(){
                 $(this).prev().children('.icon').html('<span class="icon material-symbols-outlined">keyboard_arrow_up</span>');
             });
-        }
+        }*/
     });
 });
