40491c0adb8a703443c522a1d5ca1233476ff522
DESKTOP-NKT48T6\subin <websera12@gmail.com>
2023-12-01 18:34:21 +0900

    마이페이지 - 내가쓴댓글, 내가 쓴 글 진행 중

--- src/main/java/zipchelin/mypage/MypageVO.java
+++ src/main/java/zipchelin/mypage/MypageVO.java
@@ -1,5 +1,19 @@
 package zipchelin.mypage;
 
-public class MypageVO {
+import java.sql.Date;
 
+public class MypageVO {
+	int reply_num;
+	int consult_num;
+	int myRe_num;
+	int reply_up;
+	int depth;
+	int order;
+	int groupNum;
+	String post_id;
+	String reply_id;
+	String consult_title;
+	String myRe_title;
+	String reply_content;
+	Date reply_date;	
 }
